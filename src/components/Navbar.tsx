c6e3110b8e77695cccd2b850709ae1105a40ff49
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-06-14 03:06:38 +0100

    🧭 Navbar done ✅

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,40 @@
 import styles from "../styles/Navbar.module.scss";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const screenwidth = 100;
+
+  const Modal = () => {
+    return (
+      <div className={styles.modal}>
+        {screenwidth < 200 ? (
+          <div className={styles.modal_height}>
+            <div className={styles.modal_title}>
+              <p>Pages</p>
+            </div>
+            <div className={styles.modal_item}>
+              <p>Home</p>
+            </div>
+            <div className={styles.modal_item}>
+              <p>Library</p>
+            </div>
+            <div className={styles.modal_item}>
+              <p>Stats</p>
+            </div>
+            <div className={styles.modal_item}>
+              <p>Export</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className={styles.modal_item}></div>
+            <div className={styles.modal_item}></div>
+          </>
+        )}
+      </div>
+    );
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.navbarWidth}>
@@ -10,7 +43,10 @@ export default function Navbar() {
           <p>KTON</p>
         </div>
         <div className={styles.navigationButtons}>
-          <p>Menu</p>
+          <span className={styles.hoverMenu}>
+            <p>Menu</p>
+            <Modal />
+          </span>
           <p>Login</p>
         </div>
       </div>
