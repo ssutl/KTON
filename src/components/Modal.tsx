726e638c36bf22348b627c992ddd11fa16ac5f49
Shay <shayc.campbell@gmail.com>
2023-07-29 04:01:20 +0100

    BookPage modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,6 @@ import React, {
 } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/Modal.module.scss";
-import useOutsideAlerter from "@/helpers/ClickOutsideFunction";
 import userAuthenticated from "@/helpers/UserAuthenticated";
 import { KTON_CONTEXT } from "../context/KTONContext";
 import genreColors from "@/helpers/sortGenreColors";
@@ -16,9 +15,14 @@ import HandleChanges from "@/helpers/HandleChanges";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 interface ModalProps {
-  specific_type: "Book_Search" | "Filter_Search" | "Select" | "Add_Genre";
+  specific_type:
+    | "Book_Search"
+    | "Filter_Search"
+    | "Select"
+    | "Add_Genre"
+    | "Type_Save";
   closeModal: () => void;
-  mainBooks: Book[];
+  mainBooks?: Book[];
   onItemClick?: (item: any) => void;
   data?: string[];
   mainBook?: Book;
@@ -39,7 +43,7 @@ const Modal = ({
   const [searchValue, setSearchValue] = useState("");
   const [restrictions, setRestricitons] = useState<boolean>(true);
   const [selectedFilter, setSelectedFilter] = useState<string | undefined>();
-  const { addGenreToBook, addGenreToUser } = HandleChanges();
+  const { addGenreToBook, addGenreToUser, updateBookCover } = HandleChanges();
   const [randomColor, setRandomColor] = useState(randomColorGenerator());
   const [selectedSort, setSelectedSort] = useState<
     "Recent" | "Rating" | "Oldest"
@@ -59,7 +63,8 @@ const Modal = ({
 
   const filteredData =
     specific_type === "Book_Search"
-      ? mainBooks
+      ? mainBooks &&
+        mainBooks
           .slice(
             0,
             restrictions ? Math.round(mainBooks.length / 2) : mainBooks.length
@@ -68,7 +73,7 @@ const Modal = ({
             book.title.toLowerCase().includes(searchValue.toLowerCase())
           )
       : specific_type === "Filter_Search"
-      ? [
+      ? mainBooks && [
           ...new Set(
             mainBooks
               .map((book) => book.genre)
@@ -90,9 +95,7 @@ const Modal = ({
         className={`${styles.modal} ${styles[specific_type]}`}
         ref={modalRef}
       >
-        {(specific_type === "Book_Search" ||
-          specific_type === "Filter_Search" ||
-          specific_type === "Add_Genre") && (
+        {specific_type !== "Select" && (
           <div className={styles.searchSection}>
             <input
               type="text"
@@ -101,103 +104,107 @@ const Modal = ({
             />
           </div>
         )}
-        {filteredData.map((eachItem, i) => (
-          <div
-            key={i}
-            className={`${styles.listItem} ${
-              selectedFilter === eachItem || selectedSort === eachItem
-                ? styles.selectedItem
-                : ""
-            } ${styles.spaceBetween}`}
-            onClick={() => {
-              if (
-                typeof eachItem !== "string" &&
-                specific_type === "Book_Search"
-              ) {
-                //If type is book search, then redirect to book page
-                router.push(`/Book/${eachItem._id}`);
-              } else if (
-                specific_type === "Filter_Search" &&
-                typeof eachItem === "string"
-              ) {
-                //If type is filter search, then set the filter
-                if (selectedFilter === eachItem) {
-                  setSelectedFilter(undefined);
-                  onItemClick!(undefined);
-                } else {
-                  setSelectedFilter(eachItem);
-                  onItemClick!(eachItem);
-                }
-              } else if (
-                specific_type === "Select" &&
-                typeof eachItem === "string"
-              ) {
-                if (selectedSort === eachItem && selectedSort !== "Recent") {
-                  setSelectedSort("Recent");
-                  onItemClick!("Recent");
-                } else if (
-                  selectedSort === eachItem &&
-                  selectedSort === "Recent"
+        {specific_type !== "Type_Save" &&
+          filteredData &&
+          filteredData.map((eachItem, i) => (
+            <div
+              key={i}
+              className={`${styles.listItem} ${
+                selectedFilter === eachItem || selectedSort === eachItem
+                  ? styles.selectedItem
+                  : ""
+              } ${styles.spaceBetween}`}
+              onClick={() => {
+                if (
+                  typeof eachItem !== "string" &&
+                  specific_type === "Book_Search"
                 ) {
-                  setSelectedSort("Oldest");
-                  onItemClick!("Oldest");
-                } else {
-                  setSelectedSort(
-                    eachItem as SetStateAction<"Recent" | "Rating" | "Oldest">
-                  );
-                  onItemClick!(eachItem);
-                }
-              } else if (
-                specific_type === "Add_Genre" &&
-                typeof eachItem === "string" &&
-                mainBook
-              ) {
-                if (!mainBook.genre.includes(eachItem)) {
-                  addGenreToBook({
-                    type: "add",
-                    data: eachItem,
-                    book_id: mainBook._id,
-                  });
-                }
-              }
-            }}
-          >
-            {(specific_type === "Filter_Search" ||
-              specific_type === "Add_Genre") &&
-            typeof eachItem === "string" &&
-            userinfo ? (
-              <>
-                <div
-                  style={
-                    {
-                      "--background-color": colorConverter(
-                        userinfo.genres[eachItem]
-                      ),
-                    } as React.CSSProperties
+                  //If type is book search, then redirect to book page
+                  router.push(`/Book/${eachItem._id}`);
+                } else if (
+                  specific_type === "Filter_Search" &&
+                  typeof eachItem === "string"
+                ) {
+                  //If type is filter search, then set the filter
+                  if (selectedFilter === eachItem) {
+                    setSelectedFilter(undefined);
+                    onItemClick!(undefined);
+                  } else {
+                    setSelectedFilter(eachItem);
+                    onItemClick!(eachItem);
                   }
-                  className={styles.tag}
-                >
-                  <p>{eachItem}</p>
-                </div>
-                {specific_type === "Add_Genre" && mainBook && (
-                  <DeleteOutlineIcon
-                    id={styles.trashIcon}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      addGenreToUser({
-                        type: "remove",
-                        data: eachItem,
-                        book_id: mainBook._id,
-                      });
-                    }}
-                  />
-                )}
-              </>
-            ) : (
-              <p>{typeof eachItem !== "string" ? eachItem.title : eachItem}</p>
-            )}
-          </div>
-        ))}
+                } else if (
+                  specific_type === "Select" &&
+                  typeof eachItem === "string"
+                ) {
+                  if (selectedSort === eachItem && selectedSort !== "Recent") {
+                    setSelectedSort("Recent");
+                    onItemClick!("Recent");
+                  } else if (
+                    selectedSort === eachItem &&
+                    selectedSort === "Recent"
+                  ) {
+                    setSelectedSort("Oldest");
+                    onItemClick!("Oldest");
+                  } else {
+                    setSelectedSort(
+                      eachItem as SetStateAction<"Recent" | "Rating" | "Oldest">
+                    );
+                    onItemClick!(eachItem);
+                  }
+                } else if (
+                  specific_type === "Add_Genre" &&
+                  typeof eachItem === "string" &&
+                  mainBook
+                ) {
+                  if (!mainBook.genre.includes(eachItem)) {
+                    addGenreToBook({
+                      type: "add",
+                      data: eachItem,
+                      book_id: mainBook._id,
+                    });
+                  }
+                }
+              }}
+            >
+              {(specific_type === "Filter_Search" ||
+                specific_type === "Add_Genre") &&
+              typeof eachItem === "string" &&
+              userinfo ? (
+                <>
+                  <div
+                    style={
+                      {
+                        "--background-color": colorConverter(
+                          userinfo.genres[eachItem]
+                        ),
+                      } as React.CSSProperties
+                    }
+                    className={styles.tag}
+                  >
+                    <p>{eachItem}</p>
+                  </div>
+                  {specific_type === "Add_Genre" && mainBook && (
+                    <DeleteOutlineIcon
+                      id={styles.trashIcon}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        addGenreToUser({
+                          type: "remove",
+                          data: eachItem,
+                          book_id: mainBook._id,
+                        });
+                      }}
+                    />
+                  )}
+                </>
+              ) : (
+                <p>
+                  {typeof eachItem !== "string" ? eachItem.title : eachItem}
+                </p>
+              )}
+            </div>
+          ))}
         {userinfo &&
           mainBook &&
           specific_type === "Add_Genre" &&
@@ -230,7 +237,27 @@ const Modal = ({
               </div>
             </div>
           )}
+        {specific_type === "Type_Save" && (
+          <div className={styles.buttonSection}>
+            <p
+              className={styles.button}
+              onClick={() => {
+                if (searchValue.length) {
+                  updateBookCover({
+                    book_id: mainBook!._id,
+                    data: searchValue,
+                  });
+                } else {
+                  alert("Invalid image URL");
+                }
+              }}
+            >
+              Save
+            </p>
+          </div>
+        )}
       </div>
+
       <div className={styles.modalBackground} onClick={() => closeModal()} />
     </>
   );
