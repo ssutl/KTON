22a2d09efe37f1ce943a089eff6235854ba47a72
Shay <shayc.campbell@gmail.com>
2023-08-04 06:15:03 +0100

    Fix redirect

--- src/helpers/AllowedRoute.ts
+++ src/helpers/AllowedRoute.ts
@@ -11,7 +11,7 @@ const AllowedRoute = () => {
     //If they dont have both and there not on verify page send them to login.
     //Use regex to check if they are on verify page as it has a dynamic route
   } else if (!clippings && !authToken) {
-    Router.push("/login");
+    Router.push("/");
     return false;
   }
 };
