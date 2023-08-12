1eec730b4b803b89392d5954547c62fc486da861
CJC <curtisjcampbell@mail.com>
2023-08-13 00:29:45 +0100

    feat: adds error boundary component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import "../styles/globals.scss";
-import type { AppProps } from "next/app";
-import React from "react";
-import Layout from "@/components/Layout";
-import { KTON_Provider } from "@/context/KTONContext";
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import React from 'react';
+import Layout from '@/components/Layout';
+import { KTON_Provider } from '@/context/KTONContext';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <KTON_Provider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </KTON_Provider>
-  );
+	return (
+		<KTON_Provider>
+			<Layout>
+				<ErrorBoundary>
+					<Component {...pageProps} />
+				</ErrorBoundary>
+			</Layout>
+		</KTON_Provider>
+	);
 }
