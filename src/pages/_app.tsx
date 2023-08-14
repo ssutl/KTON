0760758bf42f98d34d1a637b5f08fd1835a3563d
Shay Campbell <shayc.campbell@gmail.com>
2023-08-14 16:58:42 +0100

    Testing out blank screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,33 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Layout from "@/components/Layout/Layout";
 import { KTON_Provider } from "@/context/KTONContext";
-import { set } from "lodash";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isMobileLandScape, setIsMobileLandScape] = useState(false);
+  const [isMobileLandscape, setIsMobileLandscape] = React.useState(false);
 
   useEffect(() => {
-    function handleOrientationChange() {
-      console.log("orientationchange");
-      if (
-        window.matchMedia("(orientation: landscape)").matches &&
-        !window.matchMedia("(hover: none)").matches &&
-        window.matchMedia("(pointer: coarse)").matches
-      ) {
-        setIsMobileLandScape(true);
-      } else {
-        setIsMobileLandScape(false);
-      }
+    if (
+      window.matchMedia("(orientation: landscape)").matches &&
+      window.matchMedia("(hover: none)").matches
+    ) {
+      setIsMobileLandscape(true);
     }
-
-    // Listen for the orientationchange event and call the handler function
-    window.addEventListener("orientationchange", handleOrientationChange);
-
-    // Call the handler function initially to check the current orientation
-    handleOrientationChange();
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
-    };
   }, []);
+
+  if (isMobileLandscape) {
+    return (
+      <div>
+        <h1>请使用竖屏浏览</h1>
+      </div>
+    );
+  }
 
   return (
     <KTON_Provider>
       <Layout>
-        {isMobileLandScape ? (
-          <div>
-            <h1>请使用竖屏浏览</h1>
-          </div>
-        ) : (
-          <Component {...pageProps} />
-        )}
+        <Component {...pageProps} />
       </Layout>
     </KTON_Provider>
   );
