ce579356f899205df2f9a6db33eead8e08ffd108
Shay Campbell <shayc.campbell@gmail.com>
2023-08-15 02:07:30 +0100

    Removing rotate logic whilst developing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,41 +5,41 @@ import Layout from "@/components/Layout/Layout";
 import { KTON_Provider } from "@/context/KTONContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isMobileLandscape, setIsMobileLandscape] = useState(false);
+  // const [isMobileLandscape, setIsMobileLandscape] = useState(false);
 
-  useEffect(() => {
-    if (
-      window.matchMedia("(orientation: landscape)").matches &&
-      window.matchMedia("(hover: none)").matches
-    ) {
-      setIsMobileLandscape(true);
-    } else {
-      setIsMobileLandscape(false);
-    }
+  // useEffect(() => {
+  //   if (
+  //     window.matchMedia("(orientation: landscape)").matches &&
+  //     window.matchMedia("(hover: none)").matches
+  //   ) {
+  //     setIsMobileLandscape(true);
+  //   } else {
+  //     setIsMobileLandscape(false);
+  //   }
 
-    screen.orientation.addEventListener("change", (event) => {
-      //make window reload
-      window.location.reload();
-    });
-  }, []);
+  //   screen.orientation.addEventListener("change", (event) => {
+  //     //make window reload
+  //     window.location.reload();
+  //   });
+  // }, []);
 
-  if (isMobileLandscape) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          height: "100dvh",
-          width: "100vw",
-        }}
-      >
-        <h1>请使用竖屏浏览</h1>
-        <p>(please use portrait mode to browse)</p>
-      </div>
-    );
-  }
+  // if (isMobileLandscape) {
+  //   return (
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //         flexDirection: "column",
+  //         height: "100dvh",
+  //         width: "100vw",
+  //       }}
+  //     >
+  //       <h1>请使用竖屏浏览</h1>
+  //       <p>(please use portrait mode to browse)</p>
+  //     </div>
+  //   );
+  // }
 
   return (
     <KTON_Provider>
