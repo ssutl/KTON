916a66f306709f82d0002a6d2f0fbac7e469cffc
Shay Campbell <shayc.campbell@gmail.com>
2023-08-18 19:38:42 +0100

    Working

--- src/helpers/Books_AllHighlights.ts
+++ src/helpers/Books_AllHighlights.ts
@@ -5,20 +5,22 @@ export default function Books_AllHighlights(books: Book[] | undefined) {
     const books_highlights: Meta_con_highlight[] = [];
 
     //Converting the clippings to meta_con_highlight[] form
-    books.map((eachBook) => {
-      eachBook.highlights
-        .sort(function (a: any, b: any) {
-          return new Date(b.Date).getTime() - new Date(a.Date).getTime();
-        })
-        .map((eachHiglight) =>
-          books_highlights.push({
-            title: eachBook.title,
-            author: eachBook.author,
-            highlight: eachHiglight,
-            book_id: eachBook._id,
+    books
+      .filter((eachBook) => eachBook.deleted === false)
+      .map((eachBook) => {
+        eachBook.highlights
+          .sort(function (a: any, b: any) {
+            return new Date(b.Date).getTime() - new Date(a.Date).getTime();
           })
-        );
-    });
+          .map((eachHiglight) =>
+            books_highlights.push({
+              title: eachBook.title,
+              author: eachBook.author,
+              highlight: eachHiglight,
+              book_id: eachBook._id,
+            })
+          );
+      });
 
     return books_highlights;
   }
