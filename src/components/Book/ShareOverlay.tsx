9f20d9f6294b343af1327d3162affe1d1196acfa
Shay Campbell <shayc.campbell@gmail.com>
2023-08-19 03:09:02 +0100

    wATERMARK IS PERM

--- src/components/Book/ShareOverlay.tsx
+++ src/components/Book/ShareOverlay.tsx
@@ -39,7 +39,8 @@ const ShareOverlay = ({
   highlightText,
   index,
 }: ShareOverlayProps) => {
-  const { books } = useContext(KTON_CONTEXT);
+  const { books, userinfo } = useContext(KTON_CONTEXT);
+  console.log("userinfo", userinfo);
   const router = useRouter();
   const book_id = router.query.id;
   const book = books?.find((book) => book._id === book_id);
@@ -47,6 +48,7 @@ const ShareOverlay = ({
   const ref = useRef<HTMLDivElement>(null);
   //Styling for the image which is passed up from the edit modal
   const [ImageStyles, setImageStyles] = useState<ImageStyles>();
+  console.log("ImageStyles", ImageStyles);
   const [TextStyles, setTextStyles] = useState<TextStyles>();
   const [MetaDataStyles, setMetaDataStyles] = useState<MetaDataStyles>();
 
@@ -65,6 +67,27 @@ const ShareOverlay = ({
           style={ImageStyles}
           contentEditable="true"
         >
+          {!userinfo?.subscription && (
+            <h1
+              id={styles.watermark}
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%) rotate(-45deg)", // Rotate the element by -45 degrees
+                color: "black", // Adjust the color and opacity as needed
+                fontSize: `${
+                  ImageStyles &&
+                  (
+                    parseFloat(ImageStyles.width.replace(/[^0-9]/g, "")) * 0.25
+                  ).toString()
+                }px`,
+                opacity: 0.1,
+              }}
+            >
+              KTON.XYZ
+            </h1>
+          )}
           <h1 style={TextStyles}>&quot;{highlightText}&quot;</h1>
           <div className={styles.imageMetaData}>
             <p style={MetaDataStyles}>{book.title}</p>
