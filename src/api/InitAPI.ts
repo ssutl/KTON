d852ae7e8352e8704979fcb35b77e70f121317ae
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-10-01 00:33:55 +0100

    Console log

--- src/api/InitAPI.ts
+++ src/api/InitAPI.ts
@@ -99,11 +99,11 @@ function InitAPI() {
     const Demo = localStorage.getItem("Demo") === "true";
     const authToken = localStorage.getItem("token");
 
-    if (authToken) {
-      fetchData();
-    } else if (Demo) {
+    if (Demo) {
       updateUserInfo(demoUserInfo);
       updateBooks(demoBooks);
+    } else if (authToken) {
+      fetchData();
     }
   }
 
