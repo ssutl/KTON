3e80d4c146ebc8080c9389498ee42f770c85f88f
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-10-25 17:39:53 +0100

    Hiding heatmap on ios

--- src/components/Home/HeatMapBanner.tsx
+++ src/components/Home/HeatMapBanner.tsx
@@ -35,6 +35,23 @@ const HeatMapBanner = () => {
     };
   }, []);
 
+  function iOS() {
+    return (
+      [
+        "iPad Simulator",
+        "iPhone Simulator",
+        "iPod Simulator",
+        "iPad",
+        "iPhone",
+        "iPod",
+      ].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    );
+  }
+
+  if (iOS()) return null;
+
   if (!highlights || !screenWidth)
     return (
       <div className={styles.HeatMapBanner}>
